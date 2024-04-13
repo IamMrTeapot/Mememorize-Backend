727aeb2e49f8683604bf891272368c937f830a86
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-04-13 20:26:10 +0700

    fix: access before init

--- controllers/memes.controllers.js
+++ controllers/memes.controllers.js
@@ -2,8 +2,8 @@ const Meme = require("../models/memes.model");
 
 exports.getMemes = async (req, res, next) => {
   try {
-    const Memes = await Meme.find();
-    res.status(200).json({ success: true, data: Memes });
+    const memes = await Meme.find();
+    res.status(200).json({ success: true, data: memes });
   } catch (error) {
     res.status(500).json({ success: false, message: error });
   }
@@ -13,8 +13,8 @@ exports.createMeme = async (req, res, next) => {
   let { name, url, description } = req.body;
   console.log(name, url, description);
   try {
-    const Meme = await Meme.create({ name, url, description });
-    res.status(201).json({ success: true, data: Meme });
+    const newMeme = await Meme.create({ name, url, description });
+    res.status(201).json({ success: true, data: newMeme });
   } catch (error) {
     console.log(error);
     res.status(500).json({ success: false, message: error });

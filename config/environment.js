5d85b333cc84dad6f213365a7b490eaf4af83807
its144prelationship <pastalonely@gmail.com>
2024-04-18 01:24:05 +0700

    feat: s3 and cloudfront environment

--- config/environment.js
+++ config/environment.js
@@ -5,4 +5,11 @@ dotenv.config();
 exports.environment = {
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/mememorize",
   port: process.env.PORT || 3000,
+  s3: {
+    bucketName: process.env.S3_BUCKET_NAME,
+    region: process.env.S3_BUCKET_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+  cloudfront: process.env.CLOUDFRONT_URL,
 };
